66f72827d0921866bd55edac16c3c658f69b4121
vasyl-kozar <kozar.vasyl@gmail.com>
2018-01-22 22:21:44 +0200

    Added additional Object Models. Extended DataHelper.

---
+++ API_TEST/DataObjects/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace API_TEST.DataObjects
+{
+    public class Comment
+    {
+        public int PpostId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Eemail { get; set; }
+        public string Body { get; set; }
+    }
+}
--- API_TEST/TestDataAccess/DataHelper.cs
+++ API_TEST/TestDataAccess/DataHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 
@@ -6,11 +7,32 @@ namespace API_TEST.TestDataAccess
 {
     public class DataHelper
     {
+        /// <summary>
+        /// Method reads data from file and converts them to object
+        /// </summary>
+        /// <typeparam name="T">Type of expected object</typeparam>
+        /// <param name="fileName">Name of file</param>
+        /// <returns>Proper object</returns>
         public static T ReadObjectFromFile<T>(string fileName)
         {
             string FilePath = ConfigurationManager.AppSettings.Get("DataPath") + "\\" + fileName;
 
             T fileObject = JsonConvert.DeserializeObject<T>(File.ReadAllText(@FilePath));
+
+            return fileObject;
+        }
+
+        /// <summary>
+        /// Method reads data from file and returns List of objects
+        /// </summary>
+        /// <typeparam name="T">Type of expected objects</typeparam>
+        /// <param name="fileName">Name of file</param>
+        /// <returns></returns>
+        public static List<T> ReadListObjectsFromFile<T>(string fileName)
+        {
+            string FilePath = ConfigurationManager.AppSettings.Get("DataPath") + "\\" + fileName;
+
+            List<T> fileObject = JsonConvert.DeserializeObject<List<T>>(File.ReadAllText(@FilePath));
 
             return fileObject;
         }
